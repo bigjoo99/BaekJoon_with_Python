fbd5890b7f0901b9b75ff9d6f92ed508750247ef
Bigjoo <ksoark0108@gmail.com>
2024-02-20 23:23:59 +0900

    24_02_20

---
+++ BaekJoon/1449.py
@@ -0,0 +1,14 @@
+n , l = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+arr.sort()
+cnt = 0
+x = arr[0]
+
+for i in arr[1:]:
+    if (i + 0.5) - (x-0.5) > l:
+        cnt += 1
+        x = i
+        
+print(cnt)
