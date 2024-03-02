8feafc4d445977c2cd807d74f70a66eab5a2aff7
Bigjoo <ksoark0108@gmail.com>
2024-03-03 00:03:13 +0900

    24_03_03

--- BaekJoon/3273.py
+++ BaekJoon/3273.py
@@ -1,13 +1,22 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
-
 arr = list(map(int,input().split()))
-
 x = int(input())
 cnt = 0
 
-for i in range(n):
-    for j in range(i+1,n):
-        if arr[i]+arr[j] == x:
-            cnt +=1
+arr.sort()
+start = 0
+end = n-1
 
+while start < end:
+    sum = arr[start] + arr[end]
+    if sum == x: 
+        cnt += 1
+        start += 1
+    elif sum < x: 
+        start+=1
+    else:
+        end-=1
 print(cnt)
